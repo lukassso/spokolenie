51a547a3aa300f54cf8878f459ec0ea39e284b4d
errhal <errhal@gmail.com>
2019-04-06 13:17:29 +0200

    Initial structure.

---
+++ src/main/java/pl/spokolenie/spokolenie/SpokolenieApplication.java
@@ -0,0 +1,13 @@
+package pl.spokolenie.spokolenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpokolenieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpokolenieApplication.class, args);
+    }
+
+}
