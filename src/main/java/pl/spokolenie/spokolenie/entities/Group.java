bd9c6c39edd04c68560dd2ffb1656a3245171345
radekRadekk <radek011999.troszyn@gmail.com>
2019-04-06 14:11:20 +0200

    Group class modification

--- src/main/java/pl/spokolenie/spokolenie/entities/Group.java
+++ src/main/java/pl/spokolenie/spokolenie/entities/Group.java
@@ -3,16 +3,23 @@ package pl.spokolenie.spokolenie.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import java.util.ArrayList;
 
 @Getter
 @Setter
 @Entity
 public class Group {
+    @Column
     private String name;
+    @Column
     private String description;
+    @Column
     private User admin;
+    @ManyToMany
     private ArrayList<User> members;
 
     public void addMember(User user) {
