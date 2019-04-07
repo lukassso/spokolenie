1c2497fba510316b696097977c1cc3b6ff54b13f
errhal <errhal@gmail.com>
2019-04-07 13:49:04 +0200

    Tworzenie grupy

--- src/main/java/pl/spokolenie/controllers/GroupController.java
+++ src/main/java/pl/spokolenie/controllers/GroupController.java
@@ -3,24 +3,35 @@ package pl.spokolenie.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import pl.spokolenie.entities.Chat;
 import pl.spokolenie.entities.Group;
 import pl.spokolenie.entities.User;
+import pl.spokolenie.repositories.ChatRepository;
+import pl.spokolenie.repositories.GroupRepository;
 import pl.spokolenie.repositories.UserRepository;
 
+import javax.transaction.Transactional;
 import java.util.HashSet;
 
-@Controller
+@RestController
 public class GroupController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/createGroup")
-    public Group createGroup(@RequestParam("name") String name, @RequestParam("description") String description) {
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private ChatRepository chatRepository;
+
+    @RequestMapping(name = "/group/create", method = RequestMethod.POST)
+    public Group createGroup(@RequestParam("name") String name, @RequestParam("description") String description,
+                             @RequestParam("location") String location, @RequestParam("photo") String photo) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentUserEmail = authentication.getName();
         if (!userRepository.findByEmailAdress(currentUserEmail).isPresent())
@@ -30,9 +41,12 @@ public class GroupController {
         Group newGroup = new Group();
         newGroup.setName(name);
         newGroup.setDescription(description);
-        newGroup.setChat(new Chat());
+        Chat chat = new Chat();
+        newGroup.setChat(chatRepository.save(chat));
+        newGroup.setLocation(location);
+        newGroup.setPhoto(photo);
         newGroup.setAdmin(currentUser);
         newGroup.setMembers(new HashSet<>());
-        return newGroup;
+        return groupRepository.save(newGroup);
     }
 }
